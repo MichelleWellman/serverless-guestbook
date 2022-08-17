bcd6ebec1d0d594a896a07d7f48bb4d7669d0d28
Michelle Wellman <51219249+MichelleWellman@users.noreply.github.com>
2022-08-17 11:03:12 -0400

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://0ad87a45.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
